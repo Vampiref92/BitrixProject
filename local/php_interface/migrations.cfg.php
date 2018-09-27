ca029ec8efc5d0a671f5f4650e5c6ef0af18f497
Sergey Frolov <frolov@adv.ru>
2018-09-27 22:24:07 +0300

    Настрйоки битиркс проекта

---
+++ local/php_interface/migrations.cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'                  => 'PetrolTrade',
+    'migration_extend_class' => '\Vf92\BitrixUtils\Migration\SprintMigrationBase',
+    'stop_on_errors'         => true,
+];
