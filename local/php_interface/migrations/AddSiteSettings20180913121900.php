37958e747aaee3ee6bf700a9049feb2fb39e51ec
frolov <sergeyfolov@inetsys.ru>
2018-10-29 18:41:10 +0300

    [master] up settings migration

--- local/php_interface/migrations/AddSiteSettings20180913121900.php
+++ local/php_interface/migrations/AddSiteSettings20180913121900.php
@@ -2,9 +2,12 @@
 
 namespace Sprint\Migration;
 
+use Bitrix\Main\Config\Configuration;
 use Bitrix\Main\Config\Option;
+use Bitrix\Main\GroupTable;
 use Bitrix\Main\SiteDomainTable;
 use Bitrix\Main\SiteTable;
+use Vf92\BitrixUtils\Config\Dbconn;
 use Vf92\BitrixUtils\Migration\SprintMigrationBase;
 use Vf92\MiscUtils\EnvType;
 
@@ -110,10 +113,101 @@ class AddSiteSettings20180913121900 extends SprintMigrationBase
         }
 
         $this->log()->info('Настройк модулей успешно изменены');
+
+        /** настройка повышенного уровня группы администраторов */
+        $securityPolice = [
+            'SESSION_TIMEOUT'      => '15',
+            'SESSION_IP_MASK'      => '255.255.255.255',
+            'MAX_STORE_NUM'        => '1',
+            'STORE_IP_MASK'        => '255.255.255.255',
+            'STORE_TIMEOUT'        => '4320',
+            'CHECKWORD_TIMEOUT'    => '60',
+            'PASSWORD_LENGTH'      => '10',
+            'PASSWORD_UPPERCASE'   => 'Y',
+            'PASSWORD_LOWERCASE'   => 'Y',
+            'PASSWORD_DIGITS'      => 'Y',
+            'PASSWORD_PUNCTUATION' => 'Y',
+            'LOGIN_ATTEMPTS'       => '3',
+        ];
+        $data = ['SECURITY_POLICY' => serialize($securityPolice)];
+        GroupTable::update(1, $data);
+
+        /** включить ограничение работы во фреймах */
+        \CSecurityFrame::SetActive(true);
+
+        /** защита редиректов от фишинга */
+        \CSecurityRedirect::SetActive(true);
+        /** Активная реакция на вторжение
+         * filter - сделать безопасными
+         * clear - очистить опасные данные
+         * none - оставить опасные данные как есть
+         */
+        Option::set('security', 'filter_action', 'clear');
+        /** добавить ip адрес атакующего в стоп-лист */
+        Option::set('security', 'filter_stop', 'N');
+        /** на сколкьо минут добавлять в стоп-лист в минутах */
+        Option::set('security', 'filter_duration', 5 * 60);
+        /** заносить попытку вторжения в лог */
+        Option::set('security', 'filter_log', 'Y');
+
+        /** Включить проактивную защиту */
+        \CSecurityFilter::SetActive(true);
+
+        /** включить веб-антивирус */
+        \CSecurityAntiVirus::SetActive(true);
+        /** действие при обнаружении вируса replace или notify_only */
+        Option::set('security', 'antivirus_action', 'replace');
+
+        /** отключаем ошибки */
+        $dbConnList = Dbconn::get();
+        $dbConnList['db']['Debug'] = false;
+        Dbconn::save($dbConnList);
+        $val = Configuration::getValue('exception_handling');
+        $val['debug'] = false;
+        Configuration::setValue('exception_handling', $val);
+
+        /** вынос временной папки за пределы док рута - папка должна существовать */
+        $dbConnList = Dbconn::get();
+        if (EnvType::isProd()) {
+            $dbConnList['define']['custom']['BX_TEMPORARY_FILES_DIRECTORY'] = realpath($_SERVER['DOCUMENT_ROOT'] . '/../../tmp/kuhniunited.ru');
+        } else {
+            $dbConnList['define']['custom']['BX_TEMPORARY_FILES_DIRECTORY'] = realpath($_SERVER['DOCUMENT_ROOT'] . '/../../tmp/dev1.kuhniunited.ru');
+        }
+        Dbconn::save($dbConnList);
+
+        /** хранение сессий в бд */
+        \CSecuritySession::activate();
+        /** смена идентификатора сессии */
+        Option::set('main', 'use_session_id_ttl', 'Y');
+        /** меняем каждые 5 минут */
+        Option::set('main', 'session_id_ttl', 5 * 60);
+
+        /** настрйоки меню */
+        /** меню для каждого сайта свое */
+        Option::set('fileman', 'different_set', 'Y');
+        $this->setTypeMenu('s1', [
+            'left'   => 'Левое меню',
+            'top'    => 'Верхнее меню',
+        ]);
+
+        $this->setPageProp('s1', [
+            'description' => 'Описание',
+            'keywords'    => 'Ключевые слова',
+        ]);
+
         return true;
     }
 
-    public function down()
+    public function setTypeMenu($siteId, $menuList)
     {
+        $moduleId = 'fileman';
+        Option::set($moduleId, 'num_menu_param', 3, false, $siteId);
+
+        SetMenuTypes($menuList, $siteId);
+    }
+
+    public function setPageProp($siteId, $propList)
+    {
+        \CFileMan::SetPropstypes($propList, false, $siteId);
     }
 }
