1062693768943fc3b48560f8cbdebb9f035da8df
frolov <sergeyfolov@inetsys.ru>
2018-11-20 10:45:34 +0300

    [master] update migrations.cfg

--- local/php_interface/migrations.cfg.php
+++ local/php_interface/migrations.cfg.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'                  => 'PetrolTrade', //Достаточно изменить только название
+    'title'                  => 'Projectname', //Достаточно изменить только название
     'migration_extend_class' => '\Vf92\BitrixUtils\Migration\SprintMigrationBase',
     'stop_on_errors'         => true,
 ];
