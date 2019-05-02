455981b6b758b04c28afe142229e91ec228fa6e4
Sergey Frolov <frolov@adv.ru>
2019-05-02 23:47:46 +0300

    [master] update annotation

---
+++ local/src/Validator/Rules/CyrillicAlpha.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Common\Validator\Rules;
+
+use Rakit\Validation\Rule;
+
+class CyrillicAlpha extends Rule
+{
+    protected $message = "Для поля доступны только кириллические символы";
+
+    public function check($value)
+    {
+        return is_string($value) && preg_match('/^[\sA-Za-zА-Яа-яЁё]+$/u', $value);
+    }
+}
